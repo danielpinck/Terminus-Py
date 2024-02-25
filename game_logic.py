922ef458dfe00096d5ffad5b5390dfe1bfc08d40
danielpinck <161208468+danielpinck@users.noreply.github.com>
2024-02-25 13:42:38 +0100

    Initial commit: Game unfinished but stable
    Initial commit: Game in unfinished but stable state.
    This commit includes the initial version of the game, which is unfinished but stable. 
    The following gameplay mechanics have been implemented: 
    Randomized room size. 
    Spawning the player at a random location.
    Spawning an object at a random location. 
    Player movement with keyboard input.
    Output of the current coordinates of the player. 
    Currently the object "?" has no function and its interaction with the player "@" is unfinished.
    Future commits will include more gameplay features, code clean up and code comments.

---
+++ game_logic.py
@@ -0,0 +1,92 @@
+import keyboard, os, random
+
+class Game:
+    def __init__(self):
+        self.player_symbol = "@"
+        self.questionmark = "?"
+        self.test_room = []
+
+    def create_matrix(self):
+        width_length = random.randint(16,20)
+        for i in range(random.randint(9,12)):
+            width_list = [" "] * width_length
+            self.test_room.append(width_list)
+
+
+
+    def build_walls(self):
+        
+        for i in range(len(self.test_room[0])):
+            self.test_room[0][i] = "#"
+            self.test_room[-1][i] = "#"
+        for j in range(len(self.test_room)):
+            self.test_room[j][0] = "#"
+            self.test_room[j][-1] = "#"
+
+    def spawn_object(self):
+        x = len(self.test_room)
+        y = len(self.test_room[0])
+        self.player_x = random.randrange(1, x-1)
+        self.player_y = random.randrange(1, y-1)
+        self.test_room[self.player_x][self.player_y] = self.player_symbol
+
+    def spawn_questionmark(self):
+        x = len(self.test_room)
+        y = len(self.test_room[0])
+        self.object_row = random.randrange(1, x-1)
+        self.object_column = random.randrange(1, y-1)
+        while self.test_room[self.object_row][self.object_column] != " ":
+            self.object_row = random.randrange(1, x-1)
+            self.object_column = random.randrange(1, y-1)
+        else:
+            self.test_room[self.object_row][self.object_column] = self.questionmark
+    
+
+
+    def move_player(self, direction):
+        player_position = self.test_room[self.player_x][self.player_y]
+        player_x_change = 0
+        player_y_change = 0
+
+        if direction == "nach-rechts" and self.test_room[self.player_x][self.player_y + 1] != "#":
+                self.test_room[self.player_x][self.player_y], self.test_room[self.player_x][self.player_y + 1] = \
+                    self.test_room[self.player_x][self.player_y + 1], self.test_room[self.player_x][self.player_y]
+                self.player_y += 1
+        elif direction == "nach-links" and self.test_room[self.player_x][self.player_y - 1] != "#":
+                self.test_room[self.player_x][self.player_y], self.test_room[self.player_x][self.player_y - 1] = \
+                    self.test_room[self.player_x][self.player_y - 1], self.test_room[self.player_x][self.player_y]
+                self.player_y -= 1
+        elif direction == "nach-unten" and self.test_room[self.player_x+1][self.player_y] != "#":
+                self.test_room[self.player_x][self.player_y] = " "
+                self.player_x += 1
+                self.test_room[self.player_x][self.player_y] = self.player_symbol
+        elif direction == "nach-oben" and self.test_room[self.player_x-1][self.player_y] != "#":
+                self.test_room[self.player_x][self.player_y] = " "
+                self.player_x -= 1
+                self.test_room[self.player_x][self.player_y] = self.player_symbol
+
+
+    def print_room(self):
+        os.system('cls')
+        print(f"row {self.player_x} column {self.player_y}")
+        for row in self.test_room:
+            print(*row)
+
+def on_key_press(event, game):
+    if event.name in ["nach-rechts", "nach-links", "nach-unten", "nach-oben"]:
+        game.move_player(event.name)
+        game.print_room()
+
+def main():
+    game = Game()
+    game.create_matrix()
+    game.build_walls()
+    game.spawn_object()
+    game.spawn_questionmark()
+    game.print_room()
+
+    keyboard.on_press(lambda event: on_key_press(event, game))
+
+    keyboard.wait("esc")
+
+
